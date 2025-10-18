8b327cedde65e0bf52c30dbae850d9e1768348ce
Rogério Barroso <rogeriobarrosodev@gmail.com>
2025-10-18 10:05:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  
+  base: "/agroclima/", 
   server: {
-    host: "::",
+    host: true,
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
