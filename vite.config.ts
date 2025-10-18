c86d800476ea04fb7ad8498399b6f9aa50174e64
Rogério Barroso <rogeriobarrosodev@gmail.com>
2025-10-18 09:50:26 -0300

    inicio

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-//import { componentTagger } from "lovable-tagger";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/agroclima/", 
   server: {
-    host: true,
+    host: "::",
     port: 8080,
   },
-  plugins: [react()], //mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
