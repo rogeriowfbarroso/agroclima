94f6f7c5fb0c2a5bd8b42d2cc6b858ea443c972a
Rogério Barroso <rogeriobarrosodev@gmail.com>
2025-10-18 07:38:58 -0300

    publicação

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/agroclima/", 
   server: {
-    host: "/agroclima/",
+    host: true,
     port: 8080,
   },
   plugins: [react()], //mode === "development" && componentTagger()].filter(Boolean),
